f661b899bb7dab4b218e2949e71007d15479cd68
Myo Zin Kyaw <myozinkyaw103@gmail.com>
2025-03-24 11:52:47 +0630

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+echo "---------- Downloading the depenencies ----------"
+composer install
+
+echo "---------- Copy .env.example ----------"
+cp .env.example .env
+
+echo "---------- Generating Environment Key ----------"
+php artisan key:generate
+
 echo "---------- Removing database/database.sqlite. ----------"
 rm -rf database/database.sqlite
 
 echo "---------- Refreshing the database ----------"
 php artisan migrate --seed
 
-echo "---------- Copy .env.example ----------"
-cp .env.example .env
-
-echo "---------- Downloading the depenencies ----------"
-composer install
-
-echo "---------- Generating Environment Key ----------"
-php artisan key:generate
+echo "---------- Generating Laravel/Passport Crypt Keys ----------"
+php artisan passport:keys
 
 echo "---------- Please, Name of the client passport ----------"
 php artisan passport:client --personal
